2c466389444a23056e4630024941c5a778a3311b
Justin Fulton <jusfulton@gmail.com>
2021-09-24 12:53:21 -0700

    some basics finished

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    //special variable that EM looks for
+    //to determine of you can do mass creations/updates
+    protected $fillable = array('first_name', 'last_name', 'age');
 }
